55cf0c45864c0f7e621b76eca3a5a678458c64a6
Shawn Elston <elstonshawn1@yahoo.com>
2021-04-06 17:45:17 -0700

    removed react-hook-form. Just moved to 7 and giving tons of trouble, even when using exact updated docs material. Added yup validation and made the form manually, which was more fun anyway.

--- src/components/form/ContactForm.js
+++ src/components/form/ContactForm.js
@@ -1,27 +1,164 @@
-import { useForm } from 'react-hook-form';
-import styled from 'styled-components';
+import styled from "styled-components";
+import * as yup from "yup";
+import { useState, useEffect } from "react";
 
-const StyledFormContainer = styled.div``
-
-
+const StyledFormContainer = styled.div``;
 
 const ContactForm = () => {
-   
-    const { register, handleSubmit, errors } = useForm();
-   
-    const onSubmit = (data) => {
-        console.log(data)
-      }
+    
+    const formSchema = yup.object().shape({
+    name: yup.string().required("Please enter your name").min(2), 
+    email: yup.string().email().required("Please enter your email"),
+    textarea: yup.string().required("Invalid message length").min(2)
+});
 
-    return (
-        <StyledFormContainer>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <label htmlFor="name">Name:</label>
-                <input {...register("name")} id="name" />
-                <input type="submit" />
-            </form>
-        </StyledFormContainer>
-    )
+const [formState, setFormState] = useState({
+    name: "", 
+    email: "",
+    textarea: "",
+});
+
+const [submitButtonDisabled, setSubmitButtonDisabled] = useState(true);
+
+const [validationErrors, setValidationErrors] = useState({
+    name: "",
+    email: "",
+    textarea: "",
+  });
+
+useEffect(() => {
+    formSchema.isValid(formState).then(valid => {
+      console.log(valid)
+        setSubmitButtonDisabled(!valid);
+    });
+  }, [formState]);
+
+    const validateChange = (event) => {
+    yup.reach(formSchema, event.target.name).validate(event.target.value)
+    .then(valid => {
+        setValidationErrors({
+            ...validationErrors,
+            [event.target.name]: ""
+        });
+    }).catch((error) => {
+        console.log(error)
+        setValidationErrors({
+            ...validationErrors,
+            [event.target.name]: error.message
+        })
+    })
 }
+  
+  const handleInputChange = (event) => {
+    event.persist();
+    const validateChangeState = {
+        ...formState,
+        [event.target.name]: event.target.value
+        
+    };
+       validateChange(event);
+       setFormState(validateChangeState);
+       console.log(event.target.value);
+    };
+  
+    const submitPostRequest =  (event) => {
+        event.preventDefault();
+        console.log('submitted');
+    };
+    
+  return (
+    <StyledFormContainer>
+        <form onSubmit={submitPostRequest}>
+            <label htmlFor="name">Name:</label>
+                <input type="text" name="name" id="name" value={formState.name} 
+                onChange={handleInputChange} />
+                            {validationErrors.name.length > 0 ? <p> {validationErrors.name}</p> : null}
+        </form>
+    </StyledFormContainer>
+  );
+};
 
 export default ContactForm
+
+
+
+
+
+
+// const ContactForm = () => {
+ 
+// const formSchema = yup.object().shape({
+//     name: yup.string().required("Please enter your name").min(2), 
+//     email: yup.string().email().required("Please enter your email"),
+//     textarea: yup.string.required("Invalid message length").min(2)
+// });
+
+// const [formState, setFormState] = useState({
+//     name: "", 
+//     email: "",
+//     textarea: "",
+// });
+
+// const [submitButtonDisabled, setSubmitButtonDisabled] = useState(true);
+
+// const [validationErrors, setValidationErrors] = useState({
+//     name: "",
+//     email: "",
+//     textarea: "",
+//   });
+
+// useEffect(() => {
+//     formSchema.isValid(formState).then(valid => {
+//       console.log(valid)
+//         setSubmitButtonDisabled(!valid);
+//     });
+//   }, [formState]);
+
+//   const validateChange = (event) => {
+//     yup.reach(formSchema, event.target.name).validate(event.target.value)
+//     .then(valid => {
+//         setValidationErrors({
+//             ...validationErrors,
+//             [event.target.name]: ""
+//         });
+//     }).catch((error) => {
+//         console.log(error)
+//         setValidationErrors({
+//             ...validationErrors,
+//             [event.target.name]: error.message
+//         })
+//     })
+// }
+  
+//   const handleInputChange = (event) => {
+//     event.persist();
+//     const validateChangeState = {
+//         ...formState,
+//         [event.target.name]: event.target.value
+        
+//     };
+//        validateChange(event);
+//        setFormState(validateChangeState);
+//        console.log(event.target.value);
+//     };
+  
+//     const submitPostRequest =  (event) => {
+//         event.preventDefault();
+//         console.log('submitted');
+//     };
+
+//   return (
+//     <StyledFormContainer>
+//         <form onSubmit={submitPostRequest}>
+//             <label htmlFor="name">Name:</label>
+//                 <input type="text" name="name" id="name" value={formState.name} 
+//                 onChange={handleInputChange} />
+//                             {validationErrors.name.length > 0 ? <p> {validationErrors.name}</p> : null}
+//         </form>
+//     </StyledFormContainer>
+//   );
+// };
+
+// export default ContactForm;
+
+
